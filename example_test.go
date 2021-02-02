aee2acebecd9e95921c3fc953920161d37874682
ouromoros <ouromoros@gmail.com>
2021-02-02 22:40:58 +0800

    Update examples

--- example_test.go
+++ example_test.go
@@ -1,6 +1,11 @@
 package borsh_test
 
-import "github.com/ouromoros/borsh-go"
+import (
+	"bytes"
+	"github.com/ouromoros/borsh-go"
+	"log"
+	"strings"
+)
 
 type A struct {
 	B int32
@@ -10,11 +15,36 @@ func ExampleDeserialize() {
 	a := A{B: 123}
 	data, _ := borsh.Serialize(a)
 	b := &A{}
-	borsh.Deserialize(b, data)
+	err := borsh.Deserialize(b, data)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func ExampleSerialize() {
 	a := A{B: 123}
+	data, err := borsh.Serialize(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(data)
+}
+
+func ExampleNewEncoder() {
+	a := A{B: 123}
+	b := strings.Builder{}
+	e := borsh.NewEncoder(&b)
+	if err := e.Encode(a); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ExampleNewDecoder() {
+	a := A{B: 123}
 	data, _ := borsh.Serialize(a)
-	print(data)
+	b := &A{}
+	d := borsh.NewDecoder(bytes.NewReader(data))
+	if err := d.Decode(b); err != nil {
+		log.Fatal(err)
+	}
 }
